4acafa3ebc3d25e0c57eac8bcf4adde58a3efcd5
paholo <paolo.arrata@gmail.com>
2021-04-04 18:25:47 -0500

    eslint config

--- src/App.test.js
+++ src/App.test.js
@@ -16,7 +16,7 @@ test("button turns Midnight Blue when clicked", () => {
 
   expect(colorButton).toHaveStyle({ backgroundColor: "MidnightBlue" });
 
-  expect(colorButton.textContent).toBe("Change to Medium Violet Red");
+  expect(colorButton).toHaveTextContent("Change to Medium Violet Red");
 });
 
 test("initial conditions", () => {
